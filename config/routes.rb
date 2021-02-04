5bbe059cf7c100c8a3cb1e885f9fc7cdcc9f2523
Hideto1112 <ueda_hideto@hotmail.com>
2021-02-04 00:34:06 +0000

    4章終了

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'posts/index', to: 'posts#index'
   get 'posts/:id', to: 'posts#show', as: :post
   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
-  post 'posts/:id/update', to: 'posts#update', as: :update_post  
+  post 'posts/:id/update', to: 'posts#update', as: :update_post
+  post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
 end
